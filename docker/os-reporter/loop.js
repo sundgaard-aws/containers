c4e1ca475ae5145e3cb60ef8dc0599ecc2d18aa2
EC2 Default User <ec2-user@ip-172-31-32-62.eu-central-1.compute.internal>
2020-10-14 15:37:32 +0000

    Update

--- docker/os-reporter/loop.js
+++ docker/os-reporter/loop.js
@@ -1,8 +1,28 @@
 var sleep = require('system-sleep');
+const { Logger } = require('aws-cloudwatch-logs')
+const AWS = require('aws-sdk');
+const createLogger = require('aws-cloudwatch-logs');
 
-console.log("loop started...");
-var loopCount = 30;
 console.clear();
+
+var cloudwatchlogs = new AWS.CloudWatchLogs({
+    apiVersion: '2014-03-28',
+    region: 'eu-central-1'
+});
+
+var defaultLogger = createLogger({
+    cloudwatchlogs: cloudwatchlogs,
+    logGroupName: '/cloud-terminal/default-logs'   // Don't forget to create LogGroups: /API/endpoints or it will fail!
+});
+var errorLogger = createLogger({
+    cloudwatchlogs: cloudwatchlogs,
+    logGroupName: '/cloud-terminal/error-logs'      // Don't forget to create LogGroups: /api/errors or it will fail!
+});
+
+defaultLogger.log("os-reporter started...");
+console.log("loop started...");
+var loopCount = 3;
+
 for(var i=0;i<loopCount;i++) {
     var output = "trade with id " + Number(i+1) + " booked";
     process.stdout.clearLine();
